15dc2e6cd98cf29f2c67662e371733c471842b89
HarshBhatia853 <harshbhatiaa09@gmail.com>
2025-04-23 01:06:44 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,11 +1,15 @@
 import random
 import datetime
 
-pairs = ["EURUSD", "GBPUSD", "USDJPY", "AUDUSD", "USDCAD", "NZDCAD", "USDCHF", "EURJPY", "GBPJPY", "AUDCAD", "AUDJPY", "CADCHF", "EURGBP"]
+# Edit this list with only real OTC pairs visible on Quotex
+pairs = ["EURUSD", "USDJPY", "NZDCAD", "AUDJPY", "EURJPY", "GBPUSD"]
 
 def generate_signal():
     pair = random.choice(pairs)
     direction = random.choice(["UP", "DOWN"])
     confidence = random.randint(90, 98)
+    
+    # Get current IST time + 30 seconds ahead
     now = (datetime.datetime.utcnow() + datetime.timedelta(seconds=19800 + 30)).strftime("%H:%M")
+    
     return f"{pair} OTC - {now} - {direction} (Confidence: {confidence}%)"
