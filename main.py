8be85fcf39339f8fd361992ffd477d6120de2c8c
HarshBhatia853 <harshbhatiaa09@gmail.com>
2025-04-23 09:36:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import threading
 BOT_TOKEN = "8000946649:AAEpgwDFFNrQhUnXVBXOdVESS-Os_RKT87g"
 bot = telebot.TeleBot(BOT_TOKEN)
 
-active_users = set()
+active_users = {}
 
 def send_signals_loop(chat_id):
-    while chat_id in active_users:
+    while active_users.get(chat_id, False):
         signal = generate_signal()
         bot.send_message(chat_id, signal)
         time.sleep(60)
@@ -17,15 +17,19 @@ def send_signals_loop(chat_id):
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     chat_id = message.chat.id
-    active_users.add(chat_id)
-    bot.send_message(chat_id, "Bot activated! You'll now receive sniper signals 30 seconds before the candle starts.")
-    threading.Thread(target=send_signals_loop, args=(chat_id,)).start()
+    if not active_users.get(chat_id):
+        active_users[chat_id] = True
+        bot.send_message(chat_id, "Bot activated! You'll now receive sniper signals 30 seconds before the candle starts.")
+        thread = threading.Thread(target=send_signals_loop, args=(chat_id,))
+        thread.daemon = True
+        thread.start()
+    else:
+        bot.send_message(chat_id, "Bot is already running!")
 
 @bot.message_handler(commands=['stop'])
 def stop_handler(message):
     chat_id = message.chat.id
-    if chat_id in active_users:
-        active_users.remove(chat_id)
-        bot.send_message(chat_id, "Bot stopped. No more signals will be sent.")
+    active_users[chat_id] = False
+    bot.send_message(chat_id, "Bot stopped. No more signals will be sent.")
 
-bot.polling()
+bot.infinity_polling()
